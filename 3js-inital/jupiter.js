af48b23c5e4768e94eb313880aa1d8d56b1bec81
shekharpareekG <shekhar.pareek@sunarctechnologies.com>
2024-05-14 14:41:09 +0530

    code push

---
+++ 3js-inital/jupiter.js
@@ -0,0 +1,95 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
+
+let webGlFunction = () =>{
+// Sizes for viewport
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+};
+
+// Camera for gemoetry
+// const camera = new THREE.PerspectiveCamera(70, sizes.width / sizes.height, 0.01, 10);
+// camera.position.z = 1;
+
+// Camera for object loader
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+camera.position.set(0, 0, 5)
+
+// const scene = new THREE.Scene();
+
+// Hear we create or import an geometry
+
+// const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+// const material = new THREE.MeshNormalMaterial();
+
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+
+// *****************************************
+
+// Add the lights in the scene
+
+const light = new THREE.PointLight(0xffffff, 50)
+light.position.set(0.8, 1.4, 1.0)
+scene.add(light)
+
+const ambientLight = new THREE.AmbientLight()
+scene.add(ambientLight)
+
+// rendering the scene into the canvas
+const container = document.getElementById('container');
+const canvas = document.querySelector('.webgl');
+container.appendChild(canvas);
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+document.body.appendChild(renderer.domElement);
+renderer.setClearColor(0x1f2937);
+
+// Orbit Controls
+const controls = new OrbitControls(camera, canvas);
+ controls.enableDamping = true // for extra properties 
+controls.target.set(0, 1, 0)
+
+// Render loop
+const loop = () => {
+  renderer.render(scene, camera);
+  controls.update();
+  window.requestAnimationFrame(loop);
+};
+loop();
+}
+
+
+
+const Btn = document.getElementById('btn');
+const btnCross = document.getElementById('crossBtn');
+Btn.addEventListener('click',function(e){
+  e.preventDefault();
+  btnCross.style.display ="inline";
+  webGlFunction ();
+  
+});
+
+const crossBtn = document.getElementById('crossBtn');
+crossBtn.addEventListener('click',function(){
+  window.location.reload();
+});
+
+
+
+// Load FBX model
+const loader = new FBXLoader();
+const scene = new THREE.Scene();
+loader.load('/assests/000.FBX', function (fbx) {
+  fbx.scale.set(0.009,0.009,0.009); 
+    scene.add(fbx);
+}, undefined, function (error) {
+    console.error(error);
+});
--- 3js-inital/main.js
+++ 3js-inital/main.js
@@ -1,58 +1,53 @@
-import * as THREE from 'three';
-import './style.css'
-import {OrbitControls} from'three/examples/jsm/controls/OrbitControls.js'
-
-// scene
-
-const scene = new THREE.Scene();
-
-// create Sphere
-
-const geometry = new THREE.SphereGeometry( 3,64,64 );
-const material = new THREE.MeshStandardMaterial ({ color: '#00ff83'} );
-
-const mesh = new THREE.Mesh(geometry,material);
-
-scene.add(mesh)
-
-// Light
-
-// const light = new THREE.PointLight( 0xffffff, 1, 100 );
-
-const light = new THREE.PointLight(0xffffcc, 2, 200);
 
 
-light.position.set(0,10,4)
-light.castShadow = true;
-scene.add(light)
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// // Scene
+// const scene = new THREE.Scene();
 
-// size for viewport
+// // Create Sphere
+// // const geometry = new THREE.SphereGeometry(3, 64, 64);
+// // const geometry = new THREE.BoxGeometry(5, 1, 5);
+// var geometry1 = new THREE.BoxBufferGeometry(1, 1, 1);
+// var geometry2 = new THREE.SphereBufferGeometry(0.5, 32, 32);
 
-const sizes = {
-  width:window.innerWidth,
-  height:window.innerHeight,
-}
+// let  mergedGeometry = new THREE.BufferGeometry();
+// mergedGeometry.merge(geometry1,0);
+// mergedGeometry.merge(geometry2,1);
 
-// Camera
+// const material = new THREE.MeshPhongMaterial({color: 'orange'});
+// const mesh = new THREE.Mesh(mergedGeometry, material);
+// scene.add(mesh);
 
-const camera = new THREE.PerspectiveCamera(45,sizes.width/sizes.height,0.1,100);
-camera.position.z =20
-scene.add(camera)
+// // Light
+// const light = new THREE.HemisphereLight('#FFFFFF', '#757575', 1.7);
+// // light.position.set(0, 10, 4);
+// // light.castShadow = true;
+// scene.add(light);
 
-// render the scene
+// // Sizes for viewport
+// const sizes = {
+//   width: window.innerWidth,
+//   height: window.innerHeight,
+// };
 
-const canvas = document.querySelector('.webgl');
-const renderer = new THREE.WebGLRenderer({canvas});
-renderer.setSize(sizes.width,sizes.height);
-renderer.render(scene,camera)
+// // Camera
+// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// camera.position.z = 5;
+// scene.add(camera);
 
-const controls = new  OrbitControls(camera,canvas);
+// // Render the scene
+// const canvas = document.querySelector('.webgl');
+// const renderer = new THREE.WebGLRenderer({ canvas });
+// renderer.setSize(sizes.width, sizes.height);
 
+// // Orbit Controls
+// const controls = new OrbitControls(camera, renderer.domElement);
 
-// loop
-
-const loop = () =>{
-renderer.render(scene,camera)
-window.requestAnimationFrame(loop)
-}
-loop()
+// // Loop
+// const loop = () => {
+//   renderer.render(scene, camera);
+//   controls.update(); // Update controls in the loop
+//   window.requestAnimationFrame(loop);
+// };
+// loop();
