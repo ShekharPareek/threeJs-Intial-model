2bc1eb2875315217b190ff46950ba1a1bce66f33
Shekhar <108127963+ShekharPareek@users.noreply.github.com>
2024-05-14 15:13:26 +0530

    Update jupiter.js

--- jupiter.js
+++ jupiter.js
@@ -1,4 +1,4 @@
-import * as THREE from './three.module.js';
+import * as THREE from './path/to/three.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 
